1ea9faeed9224e3c5012567bc894f2193ca327dc
manhninh91 <manhninh91@gmail.com>
2021-04-01 02:18:08 +0700

    finish sidebar and header

--- src/containers/components/layout/Header/index.tsx
+++ src/containers/components/layout/Header/index.tsx
@@ -1,77 +1,70 @@
-import {ReactComponent as ChevronRightSvg} from 'assets/images/ChevronRight.svg';
-import {ReactComponent as LogoSvg} from 'assets/images/logo.svg';
-import {ReactComponent as NotificationSvg} from 'assets/images/notification.svg';
-import {ReactComponent as SupportSvg} from 'assets/images/support.svg';
-import {ReactComponent as UserSvg} from 'assets/images/user.svg';
 import React from 'react';
-import {Col, Dropdown, Row} from 'react-bootstrap';
-import {DepositStyled, DepositTextStyled, LinkStyled, LinkTextStyled, LogoStyled, TypeUserTextStyled} from './styles';
-
-const CustomToggle = React.forwardRef((props: any, ref: any) => (
-  <a
-    href=""
-    ref={ref}
-    onClick={(e) => {
-      e.preventDefault();
-      props.onClick(e);
-    }}>
-    {props.children}
-    <div style={{marginLeft: 40, display: 'inline-block', position: 'relative', bottom: 15}}>
-      <ChevronRightSvg />
-    </div>
-  </a>
-));
+import './styled.css';
 
 const HeaderLayout = () => {
   return (
-    <Row>
-      <Col xs={4} md={4}>
-        <LogoStyled>
-          <LogoSvg />
-        </LogoStyled>
-      </Col>
-      <Col xs={8} md={8}>
-        <Row className="float-right">
-          <Dropdown style={{background: '#49505A', borderRadius: '0px 0px 10px 10px', padding: '0 16px 10px'}}>
-            <Dropdown.Toggle as={CustomToggle}>
-              <div style={{display: 'inline-block'}}>
-                <TypeUserTextStyled>Demo account</TypeUserTextStyled>
-                <TypeUserTextStyled>$ 10.000.000</TypeUserTextStyled>
+    <header className="header">
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid d-flex align-items-center justify-content-between">
+          <div className="navbar-header">
+            <a href="index.html" className="navbar-brand">
+              <div className="brand-big text-uppercase">
+                <strong className="logoIOGO">IOGO</strong>
+                <strong className="logoETH">ETH</strong>
               </div>
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu>
-              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-
-          <DepositStyled type="button">
-            <DepositTextStyled>DEPOSIT</DepositTextStyled>
-          </DepositStyled>
-
-          <LinkStyled href="#">
-            <div>
-              <UserSvg />
+            </a>
+          </div>
+          <div className="right-menu list-inline no-margin-bottom">
+            <div
+              className="list-inline-item dropdown"
+              style={{background: '#22252a', borderRadius: '0px 0px 10px 10px', marginRight: '2rem'}}>
+              <a
+                id="languages"
+                rel="nofollow"
+                data-target="#"
+                href="#"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+                className="nav-link language dropdown-toggle">
+                <div style={{display: 'inline-block', marginLeft: '1rem'}}>
+                  <span style={{display: 'block', marginBottom: 5}}>Demo</span>
+                  <span style={{display: 'block'}}>$ 10.000.000</span>
+                </div>
+              </a>
+              <div aria-labelledby="languages" className="dropdown-menu">
+                <a rel="nofollow" href="#" className="dropdown-item">
+                  <span>Real</span>
+                  <span>$ 0</span>
+                </a>
+              </div>
             </div>
-            <LinkTextStyled>manhninh91</LinkTextStyled>
-          </LinkStyled>
-          <LinkStyled href="#">
-            <div>
-              <NotificationSvg />
+            <div className="list-inline-item">
+              <input type="button" value="DEPOSIT" className="btn btn-primary" />
             </div>
-            <LinkTextStyled>Notification</LinkTextStyled>
-          </LinkStyled>
-          <LinkStyled href="#">
-            <div>
-              <SupportSvg />
+            <div className="list-inline-item">
+              <a id="logout" href="login.html" className="nav-link">
+                <i className="icomoon-icon-user"></i>
+                <span className="d-none d-sm-inline">Mr.Dung</span>
+              </a>
             </div>
-            <LinkTextStyled>Support</LinkTextStyled>
-          </LinkStyled>
-        </Row>
-      </Col>
-    </Row>
+            <div className="list-inline-item">
+              <a id="logout" href="login.html" className="nav-link">
+                <span className="badge dashbg-3">1</span>
+                <i className="icomoon-icon-notification"></i>
+                <span className="d-none d-sm-inline">Notification</span>
+              </a>
+            </div>
+            <div className="list-inline-item">
+              <a id="logout" href="login.html" className="nav-link">
+                <i className="icomoon-icon-support"></i>
+                <span className="d-none d-sm-inline">Support</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
   );
 };
 
