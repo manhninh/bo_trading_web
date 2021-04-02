350a395b22b94932d9379f2c8ffbe62e80cb37fe
ninh.pham <manhninh91@gmail.com>
2021-04-02 15:54:30 +0700

    update UI countdown timer

--- src/screens/trade/RightLayout/index.tsx
+++ src/screens/trade/RightLayout/index.tsx
@@ -1,31 +1,37 @@
-import {ReactComponent as DevideSvg} from 'assets/images/devide.svg';
 import React from 'react';
+import CountDownTimer from "./CountDown";
 import './styled.css';
 
 const RightLayout = () => {
+  const getRandomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
   return (
-    <div className="d-flex align-items-stretch" style={{width: '400px'}}>
-      <div className="card" style={{marginBottom: 0, width: '100%', padding: '0.5rem'}}>
-        <div className="block" style={{background: '#49505A', borderRadius: '10px'}}>
+    <div className="d-flex align-items-stretch" style={{ width: '400px' }}>
+      <div className="card" style={{ marginBottom: 0, width: '100%', padding: '0.5rem' }}>
+        <div className="block" style={{ background: '#49505A', borderRadius: '10px' }}>
           <div className="title">
             <strong className="d-block">Income</strong>
           </div>
           <div className="block-body">
             <div className="stats-2 d-flex">
               <div className="stats-2-content">
-                <strong className="d-block text-primary" style={{fontSize: 26}}>
+                <strong className="d-block text-primary" style={{ fontSize: 26 }}>
                   + 95%
                 </strong>
               </div>
               <div className="stats-2-content">
-                <strong className="d-block text-primary" style={{fontSize: 20, marginLeft: '1rem'}}>
+                <strong className="d-block text-primary" style={{ fontSize: 20, marginLeft: '1.5rem' }}>
                   + 950%
                 </strong>
               </div>
             </div>
           </div>
         </div>
-        <div className="block" style={{background: '#49505A', borderRadius: '10px'}}>
+        <div className="block">
           <div className="title">
             <strong className="d-block">Amount</strong>
           </div>
@@ -35,22 +41,23 @@ const RightLayout = () => {
             </div>
             <div className="form-group">
               <div className="input-group-append d-flex justify-content-between">
-                <button className="btn btn-secondary" style={{width: '20%'}}>
-                  <i className="fa fa-plus" />
+                <button type="button" className="btn btn-secondary btn-outline-secondary" style={{ width: '20%' }}>
+                  <i className="fas fa-plus" />
                 </button>
-                <button className="btn btn-secondary" style={{width: '20%'}}>
-                  <i className="fa fa-minus" />
+                <button type="button" className="btn btn-secondary btn-outline-secondary" style={{ width: '20%' }}>
+                  <i className="fas fa-minus" />
                 </button>
-                <button className="btn btn-secondary" style={{width: '20%'}}>
-                  <i className="fa fa-times" />
+                <button type="button" className="btn btn-secondary btn-outline-secondary" style={{ width: '20%' }}>
+                  <i className="fas fa-times" />
                 </button>
-                <button className="btn btn-secondary" style={{width: '20%'}}>
-                  <DevideSvg />
+                <button type="button" className="btn btn-secondary btn-outline-secondary" style={{ width: '20%' }}>
+                  <i className="fas fa-divide" />
                 </button>
               </div>
             </div>
           </div>
         </div>
+        <CountDownTimer />
       </div>
     </div>
   );
