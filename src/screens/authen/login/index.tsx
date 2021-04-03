8812e6656ed54cc637b1e97b0aa324c554375fb0
Phạm Ninh <manhninh91@gmail.com>
2021-04-04 01:41:10 +0700

    finish right trading and start login

--- src/screens/authen/login/index.tsx
+++ src/screens/authen/login/index.tsx
@@ -1,9 +1,10 @@
-import {yupResolver} from '@hookform/resolvers/yup';
-import {isLoading} from 'containers/redux/slice';
-import React, {useEffect} from 'react';
-import {useForm} from 'react-hook-form';
-import {useDispatch} from 'react-redux';
-import {fetchLogin} from 'routers/redux/thunks';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { ReactComponent as BackgroundSvg } from 'assets/images/back_login.svg';
+import { isLoading } from 'containers/redux/slice';
+import React, { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { fetchLogin } from 'routers/redux/thunks';
 import * as yup from 'yup';
 
 interface IFormInputs {
@@ -19,9 +20,9 @@ const schema = yup.object().shape({
 const LogInComponent = () => {
   const dispatch = useDispatch();
 
-  useEffect(() => {}, []);
+  useEffect(() => { }, []);
 
-  const {control, handleSubmit, errors} = useForm<IFormInputs>({
+  const { control, handleSubmit, errors } = useForm<IFormInputs>({
     defaultValues: {
       username: '',
       password: '',
@@ -32,14 +33,50 @@ const LogInComponent = () => {
   const onSubmit = async (data: IFormInputs) => {
     dispatch(isLoading(true));
     try {
-      await dispatch(fetchLogin({username: data.username, password: data.password}));
+      await dispatch(fetchLogin({ username: data.username, password: data.password }));
     } catch (error) {
     } finally {
       dispatch(isLoading(false));
     }
   };
 
-  return <div>login</div>;
+  return (
+    <div className="container-fluid px-3">
+      <div className="row min-vh-100 bg-gray-dark">
+        <div className="col-md-5 col-lg-6 col-xl-4 px-lg-5 d-flex align-items-center">
+          <div className="w-100 py-5">
+            <div className="text-center">
+              <h1 className="display-4 text-gray-light mb-3">Sign in</h1>
+            </div>
+            <form className="form-validate">
+              <div className="form-group">
+                <label>Email Address</label>
+                <input name="loginUsername" type="email" placeholder="name@address.com" className="form-control" />
+              </div>
+              <div className="form-group mb-4">
+                <div className="row">
+                  <div className="col">
+                    <label>Password</label>
+                  </div>
+                  <div className="col-auto"><a href="#" className="form-text small text-muted">Forgot password?</a></div>
+                </div>
+                <input name="loginPassword" placeholder="Password" type="password" className="form-control" />
+              </div>
+
+              <button className="btn btn-lg btn-block btn-primary mb-3">Sign in</button>
+
+              <p className="text-center"><small className="text-muted text-center">Don't have an account yet? <a href="register-2.html">Register</a>.</small></p>
+            </form>
+
+          </div>
+        </div>
+        <div className="col-12 col-md-7 col-lg-6 col-xl-8 d-none d-lg-block">
+
+          <BackgroundSvg height="800" className="bg-cover h-100 mr-n3" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default React.memo(LogInComponent);
--- src/screens/trade/RightLayout/index.tsx
+++ src/screens/trade/RightLayout/index.tsx
@@ -3,63 +3,63 @@ import CountDownTimer from "./CountDown";
 import './styled.css';
 
 const RightLayout = () => {
-  const getRandomInt = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
-
   return (
-    <div className="d-flex align-items-stretch" style={{ width: '400px' }}>
-      <div className="card" style={{ marginBottom: 0, width: '100%', padding: '0.5rem' }}>
-        <div className="block" style={{ background: '#49505A', borderRadius: '10px' }}>
-          <div className="title">
-            <strong className="d-block">Income</strong>
-          </div>
-          <div className="block-body">
-            <div className="stats-2 d-flex">
-              <div className="stats-2-content">
-                <strong className="d-block text-primary" style={{ fontSize: 26 }}>
-                  + 95%
+    <>
+      <div className="block block-container">
+        <div className="title">
+          <strong className="d-block">Income</strong>
+        </div>
+        <div className="block-body">
+          <div className="stats-2 d-flex">
+            <div className="stats-2-content">
+              <strong className="d-block text-primary" style={{ fontSize: 22 }}>
+                + 95000%
                 </strong>
-              </div>
-              <div className="stats-2-content">
-                <strong className="d-block text-primary" style={{ fontSize: 20, marginLeft: '1.5rem' }}>
-                  + 950%
+            </div>
+            <div className="stats-2-content">
+              <strong className="d-block text-primary" style={{ fontSize: 16, marginLeft: '.5rem' }}>
+                + 95000%
                 </strong>
-              </div>
             </div>
           </div>
         </div>
-        <div className="block">
-          <div className="title">
-            <strong className="d-block">Amount</strong>
-          </div>
-          <div className="block-body">
-            <div className="form-group">
-              <input type="email" placeholder="$ 0" className="form-control input-amount" />
-            </div>
-            <div className="form-group">
-              <div className="input-group-append d-flex justify-content-between">
-                <button type="button" className="btn btn-secondary btn-outline-secondary" style={{ width: '20%' }}>
-                  <i className="fas fa-plus" />
-                </button>
-                <button type="button" className="btn btn-secondary btn-outline-secondary" style={{ width: '20%' }}>
-                  <i className="fas fa-minus" />
-                </button>
-                <button type="button" className="btn btn-secondary btn-outline-secondary" style={{ width: '20%' }}>
-                  <i className="fas fa-times" />
-                </button>
-                <button type="button" className="btn btn-secondary btn-outline-secondary" style={{ width: '20%' }}>
-                  <i className="fas fa-divide" />
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <CountDownTimer />
       </div>
-    </div>
+      <div className="block block-container">
+        <div className="title">
+          <strong className="d-block">Amount</strong>
+        </div>
+        <div className="block-body">
+          <div className="form-group">
+            <input type="email" placeholder="$ 0" className="form-control input-amount" />
+          </div>
+          <div className="form-group" style={{ marginBottom: 0 }}>
+            <div className="input-group-append d-flex justify-content-between">
+              <button type="button" className="btn btn-secondary btn-outline-secondary">
+                <i className="fas fa-plus" />
+              </button>
+              <button type="button" className="btn btn-secondary btn-outline-secondary">
+                <i className="fas fa-minus" />
+              </button>
+              <button type="button" className="btn btn-secondary btn-outline-secondary">
+                <i className="fas fa-times" />
+              </button>
+              <button type="button" className="btn btn-secondary btn-outline-secondary">
+                <i className="fas fa-divide" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <CountDownTimer />
+      <div className="btn-group buy-sel-action">
+        <button className="btn btn-lg btn-info">
+          <i className="fas fa-chevron-circle-up" /> BUY
+          </button>
+        <button className="btn btn-lg btn-danger">
+          <i className="fas fa-chevron-circle-down" /> SELL
+          </button>
+      </div>
+    </>
   );
 };
 
