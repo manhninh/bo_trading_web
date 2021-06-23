d3073957bc613fc01a44d5ad9793433a29543de8
manhninh91 <manhninh91@gmail.com>
2021-06-23 09:59:47 +0700

    update code

--- src/screens/wallet/index.tsx
+++ src/screens/wallet/index.tsx
@@ -1,23 +1,23 @@
-import { useAppSelector } from 'boot/configureStore';
+import {useAppSelector} from 'boot/configureStore';
 import config from 'constants/config';
 import ContainerLayout from 'containers/components/layout/Container';
 import SpinnerLoader from 'containers/components/loader';
-import React, { lazy, Suspense, useCallback, useState } from 'react';
-import { Nav, NavItem, Tab, TabContent } from 'react-bootstrap';
+import React, {lazy, Suspense, useCallback, useState} from 'react';
+import {Nav, NavItem, Tab, TabContent} from 'react-bootstrap';
 import Switch from 'react-bootstrap/esm/Switch';
-import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
-import { useTranslation } from 'react-i18next';
-import { NavLink, Redirect, Route, useHistory, useLocation } from 'react-router-dom';
-import { formatter2 } from 'utils/formatter';
+import {GoogleReCaptchaProvider} from 'react-google-recaptcha-v3';
+import {useTranslation} from 'react-i18next';
+import {NavLink, Redirect, Route, useHistory, useLocation} from 'react-router-dom';
+import {formatter2} from 'utils/formatter';
 import DepositComponent from './deposit';
 import './styled.css';
 import TransferComponent from './transfer';
-import { TYPE_WALLET } from './transfer/propState';
+import {TYPE_WALLET} from './transfer/propState';
 import WithdrawComponent from './withdraw';
 
 const WalletComponent = () => {
-  const { t, i18n } = useTranslation();
-  const { pathname } = useLocation();
+  const {t, i18n} = useTranslation();
+  const {pathname} = useLocation();
   const history = useHistory();
   const amount = useAppSelector((state) => state.authState.accountInfor.amount);
   const amount_trade = useAppSelector((state) => state.authState.accountInfor.amount_trade);
@@ -37,7 +37,7 @@ const WalletComponent = () => {
 
   const onRequestRefesh = useCallback(
     (tabActive) => {
-      setState({ ...state, requestRefesh: tabActive });
+      setState({...state, requestRefesh: tabActive});
     },
     [state.requestRefesh],
   );
@@ -61,7 +61,7 @@ const WalletComponent = () => {
     ));
 
   return (
-    <GoogleReCaptchaProvider reCaptchaKey={config.GOOGLE_RECAPTCHA_SITE_KEY} scriptProps={{ async: true }}>
+    <GoogleReCaptchaProvider reCaptchaKey={config.GOOGLE_RECAPTCHA_SITE_KEY} scriptProps={{async: true}}>
       <ContainerLayout>
         <div className="row">
           <div className="col-md-6 col-xs-12">
@@ -75,7 +75,7 @@ const WalletComponent = () => {
                     {t('common:wallet.walletSpot')}
                   </h2>
                 </div>
-                <div className="number text-warning text-bold">$ {formatter2.format(amount)}</div>
+                <div className="number text-warning text-bold">{formatter2.format(amount)} USDF</div>
               </div>
               <DepositComponent />
               <TransferComponent
